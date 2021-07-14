a1a6aa3176afcef60d596cbc6306b6ee69359467
Addis Belete <addis.bel@gmail.com>
2021-07-14 17:51:41 +0300

    Change to ES6 arrow function

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,15 +5,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Button(props) {
-  return (
-    <button type="button" className="square">
-      {props.name}
-    </button>
-  );
-}
+const Button = (props) => (
+  <button type="button" className="square">
+    {props.name}
+  </button>
+);
 
 Button.propTypes = {
+
   name: PropTypes.string,
 };
 export default Button;
