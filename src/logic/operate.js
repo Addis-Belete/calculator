3e084cb4b2aa5df774bf6dc9fa3c81c683468661
Addis Belete <addis.bel@gmail.com>
2021-07-18 21:22:44 +0300

    Return invalid when number divided by zero

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-param-reassign */
 /* eslint-disable no-console */
 import Big from 'big.js';
 
@@ -13,11 +15,21 @@ const operate = (numberOne, numberTwo, operation) => {
   } else if (operation === '*') {
     result = num1.times(num2);
   } else if (operation === '÷') {
-    result = num1.div(num2);
+    result = num1.div(notZero(num2));
   } else if (operation === '%') {
     result = num1.div(100);
   }
   return result;
+};
+const notZero = (n) => {
+  const zero = document.getElementById('result');
+  const div = document.getElementsByClassName('display-result');
+  n = +n; // Coerce to number.
+  if (!n) { // Matches +0, -0, NaN
+    zero.innerHTML = `Invalid dividend ${n}`;
+    div.append(zero);
+  }
+  return n;
 };
 
 export default operate;
