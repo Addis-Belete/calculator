70949eac415b146626808512daec4729971c3aad
Addis Belete <addis.bel@gmail.com>
2021-07-19 16:34:16 +0300

    Refactor ButtonPannel.js to functional

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -2,59 +2,46 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-class ButtonPannel extends React.Component {
-  constructor(props) {
-    super(props);
+const ButtonPannel = ({ clickHandler }) => (
+  <div className="board">
+    <div className="board-row">
+      <Button name="AC" handleClick={clickHandler} />
+      <Button name="+/-" handleClick={clickHandler} />
+      <Button name="%" handleClick={clickHandler} />
+      <Button name="÷" handleClick={clickHandler} />
 
-    this.state = {
+    </div>
+    <div className="board-row">
+      <Button name="7" handleClick={clickHandler} />
+      <Button name="8" handleClick={clickHandler} />
+      <Button name="9" handleClick={clickHandler} />
+      <Button name="X" handleClick={clickHandler} />
 
-    };
-  }
+    </div>
+    <div className="board-row">
+      <Button name="4" handleClick={clickHandler} />
+      <Button name="5" handleClick={clickHandler} />
+      <Button name="6" handleClick={clickHandler} />
+      <Button name="-" handleClick={clickHandler} />
 
-  render() {
-    const { clickHandler } = this.props;
-    return (
-      <div className="board">
-        <div className="board-row">
-          <Button name="AC" handleClick={clickHandler} />
-          <Button name="+/-" handleClick={clickHandler} />
-          <Button name="%" handleClick={clickHandler} />
-          <Button name="÷" handleClick={clickHandler} />
+    </div>
+    <div className="board-row">
+      <Button name="1" handleClick={clickHandler} />
+      <Button name="2" handleClick={clickHandler} />
+      <Button name="3" handleClick={clickHandler} />
+      <Button name="+" handleClick={clickHandler} />
 
-        </div>
-        <div className="board-row">
-          <Button name="7" handleClick={clickHandler} />
-          <Button name="8" handleClick={clickHandler} />
-          <Button name="9" handleClick={clickHandler} />
-          <Button name="X" handleClick={clickHandler} />
+    </div>
+    <div className="board-row">
+      <Button name="0" handleClick={clickHandler} />
+      <Button name="." handleClick={clickHandler} />
+      <Button name="=" handleClick={clickHandler} />
 
-        </div>
-        <div className="board-row">
-          <Button name="4" handleClick={clickHandler} />
-          <Button name="5" handleClick={clickHandler} />
-          <Button name="6" handleClick={clickHandler} />
-          <Button name="-" handleClick={clickHandler} />
+    </div>
 
-        </div>
-        <div className="board-row">
-          <Button name="1" handleClick={clickHandler} />
-          <Button name="2" handleClick={clickHandler} />
-          <Button name="3" handleClick={clickHandler} />
-          <Button name="+" handleClick={clickHandler} />
+  </div>
 
-        </div>
-        <div className="board-row">
-          <Button name="0" handleClick={clickHandler} />
-          <Button name="." handleClick={clickHandler} />
-          <Button name="=" handleClick={clickHandler} />
-
-        </div>
-
-      </div>
-
-    );
-  }
-}
+);
 
 ButtonPannel.propTypes = {
   clickHandler: PropTypes.func.isRequired,
