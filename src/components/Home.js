83bcf234d3ca25093f2807da6f5c1cffcd9a831d
Addis Belete <addis.bel@gmail.com>
2021-07-20 15:20:57 +0300

    Add style to qoute.js and home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => (
   <div className="home">
     <h2>
-      Welcome to our page!HomeCalculatorQuote
+      Welcome to our page!
     </h2>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing
--- src/components/Qoute.js
+++ src/components/Qoute.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Qoute = () => (
-  <p>
+  <p className="qoute">
     Mathematics is not about numbers, equations, computations, or algorithms:
-    it is about understanding. –William Paul ThurstonHomeCalculatorQuote
+    it is about understanding. –William Paul Thurston
   </p>
 
 );
