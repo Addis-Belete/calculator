69245a303df958eea32a83190837fa872706caa4
Addis Belete <addis.bel@gmail.com>
2021-07-20 09:11:53 +0300

    Add Qoute.js file with content

---
+++ src/components/Qoute.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Qoute = () => (
+  <p>
+    Mathematics is not about numbers, equations, computations, or algorithms:
+    it is about understanding. –William Paul ThurstonHomeCalculatorQuote
+  </p>
+
+);
+export default Qoute;
