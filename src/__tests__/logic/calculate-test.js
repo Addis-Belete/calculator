cf5c9b0eea15d66111d8234332c16d5c0214d726
Addis Belete <addis.bel@gmail.com>
2021-07-21 10:54:03 +0300

    Add tests for addition subtraction multiplication and division

--- src/__tests__/logic/calculate-test.js
+++ src/__tests__/logic/calculate-test.js
@@ -24,4 +24,34 @@ describe('calculate methods', () => {
     expect(result.total).toBe(null);
     expect(result.operation).toBe(null);
   });
+  it('Should return a float value', () => {
+    const result = calculate('.', calculate1);
+    expect(result).toEqual({ next: null, operation: null, total: '0.' });
+  });
+
+  it('Should return a result of subtraction', () => {
+    const result = calculate('=', { total: '7', next: '10', operation: '-' });
+    expect(result.total * 1).toEqual(-3);
+    expect(result.next).toEqual(null);
+    expect(result.operation).toEqual(null);
+  });
+
+  it('Should return a result of the division', () => {
+    const result = calculate('=', { total: '7', next: '10', operation: '÷' });
+    expect(result.total * 1).toEqual(0.7);
+    expect(result.next).toEqual(null);
+    expect(result.operation).toEqual(null);
+  });
+  it('should return a result of addition', () => {
+    const result = calculate('=', { total: '2', next: '2', operation: '+' });
+    expect(result.total * 1).toEqual(4);
+    expect(result.next).toEqual(null);
+    expect(result.operation).toEqual(null);
+  });
+  it('should return a result of multiplication', () => {
+    const result = calculate('=', { total: '2', next: '0', operation: '*' });
+    expect(result.total * 1).toEqual(0);
+    expect(result.next).toEqual(null);
+    expect(result.operation).toEqual(null);
+  });
 });
