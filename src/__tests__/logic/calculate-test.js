0f727a8d0d69770a7d4a67182af73f0266bc534d
Addis Belete <addis.bel@gmail.com>
2021-07-21 10:41:56 +0300

    Add a test to check for -ve and percent

--- src/__tests__/logic/calculate-test.js
+++ src/__tests__/logic/calculate-test.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import calculate from '../../logic/calculator';
 
 const calculate1 = { total: null, next: null, operation: null };
@@ -6,5 +5,23 @@ const calculate2 = { total: '10', next: null, operation: null };
 describe('calculate methods', () => {
   it('should be a function', () => {
     expect(typeof calculate).toBe('function');
+  });
+  it('should return an object', () => {
+    const result = calculate(10, calculate1);
+    expect(typeof result).toEqual('object');
+  });
+  it('Should return total divide by 100', () => {
+    const result = calculate('%', calculate2);
+    expect(result.total).toEqual(0.1);
+  });
+  it('should return -ve value', () => {
+    const result = calculate('+/-', calculate2);
+    expect(result.total).toBe(-10);
+  });
+  it('should return all null', () => {
+    const result = calculate('AC', { total: '10', next: '2', operation: '*' });
+    expect(result.next).toBe(null);
+    expect(result.total).toBe(null);
+    expect(result.operation).toBe(null);
   });
 });
