8a0e053afb131b00be4004716880315e7101bb91
Addis Belete <addis.bel@gmail.com>
2021-07-21 09:59:58 +0300

    Import calculate.js to calculator-test.js

---
+++ src/__tests__/components/Button-test.js
@@ -0,0 +1,5 @@
+describe('Button', () => {
+  it('passing test', () => {
+    expect(true).toBeTruthy();
+  });
+});
---
+++ src/__tests__/logic/calculate-test.js
@@ -0,0 +1,6 @@
+import calculate from '../../logic/calculator';
+
+const calculate1 = { total: null, next: null, operation: null };
+describe('calculate', () => {
+
+});
