a9dfa114a059e2388508823fb868cbaa69133c00
Addis Belete <addis.bel@gmail.com>
2021-07-21 10:04:14 +0300

    Add a test to check if the calculate is function

--- src/__tests__/logic/calculate-test.js
+++ src/__tests__/logic/calculate-test.js
@@ -1,6 +1,10 @@
+/* eslint-disable no-unused-vars */
 import calculate from '../../logic/calculator';
 
 const calculate1 = { total: null, next: null, operation: null };
-describe('calculate', () => {
-
+const calculate2 = { total: '10', next: null, operation: null };
+describe('calculate methods', () => {
+  it('should be a function', () => {
+    expect(typeof calculate).toBe('function');
+  });
 });
