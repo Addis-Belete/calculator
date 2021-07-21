5ce83e47079ebaf32414361846c52c9daa4d4209
Addis Belete <addis.bel@gmail.com>
2021-07-21 15:27:16 +0300

    Add snapshoot test for buttonpanel.js

--- src/__tests__/components/Button-test.js
+++ src/__tests__/components/Button-test.js
@@ -1,11 +1,9 @@
-/* eslint-disable no-alert */
-/* eslint-disable no-undef */
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Button from '../../components/Button';
 
 describe('Button', () => {
-  it('renders Button', () => {
+  it('should renders Button', () => {
     const clickHandler = () => '1';
     const tree = renderer.create(<Button name="1" handleClick={clickHandler} />).toJSON();
     expect(tree).toMatchSnapshot();
